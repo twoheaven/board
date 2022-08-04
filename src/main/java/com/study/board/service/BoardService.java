0655c4e7118b1e59483b4104e9280cbedcc67b39
twoheaven <dlcjsrnr0860@naver.com>
2022-08-04 18:18:40 +0900

    게시물 리스트

--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -5,6 +5,8 @@ import com.study.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BoardService {
 
@@ -15,5 +17,10 @@ public class BoardService {
     public void write(Board board){
 
         boardRepository.save(board);
+    }
+
+    public List<Board> boardList(){
+
+        return boardRepository.findAll();
     }
 }
